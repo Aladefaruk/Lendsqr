2ef66418f2d1ccf997ec38f67f0fd80f53369788
Aladefaruk <alade.faruk11@gmail.com>
2022-10-10 08:54:49 -0700

    Netlify routing issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,14 @@ import User from "./pages/User.tsx";
 
 import GeneralDetails from "./pages/mobile/GeneralDetails.tsx";
 
-
 function App() {
   return (
     <div className="p-5">
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/user:id" element={<User />} />
-        <Route path="/user:id/details" element={<GeneralDetails />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/user/details" element={<GeneralDetails />} />
       </Routes>
     </div>
   );
